db8c3b8e9b42864fec05355893d4c4859897cf72
222842 <112934603+222842@users.noreply.github.com>
2022-09-21 10:48:30 +0900

    finished chapter2

---
+++ chapter02/c_printf_char.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * vs code 단축키
+ * Shift + Alt + N : 실행
+ * Shift + Alt + ↓ or ↑ : Line 복사
+ * Alt + ↓ or ↑ : Line 이동
+ * Ctrl + x : Line 삭제
+ * Ctrl + space : 자동완성
+*/
+int main(void)
+{
+    /* code */
+    printf("%c\n", 'A');
+    printf("%s\n", "A");
+    printf("%c은 %s입니다.\n", '1', "first");
+    return 0;
+}
