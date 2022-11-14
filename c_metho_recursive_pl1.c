b1c3b89f436b55eebac64c29e5c75342646ef457
222842 <112934603+222842@users.noreply.github.com>
2022-11-14 10:41:30 +0900

    20221114

---
+++ c_metho_recursive_pl1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int func(int num);
+int main(void) {
+    // i=5 4 3 2 1 0 → i=5 3 1
+    // i=5    출력 = func(5): f(5) → 120
+    //                       num = 5, 5 * func(4)  # 5*24=120
+    //                       num = 4, 4 * func(3)  # 4*6=24
+    //                       num = 3, 3 * func(2)  # 3*2=6
+    //                       num = 2, 2 * func(1)  # 2*1=2
+    //                       num = 1, return 1
+
+    for(int i=5; i>=0; i--) {
+        if(i % 2 ==1) printf("func(%d): %d\n", i, func(i));
+        } // 홀수일 때만 기능을 함
+    }
+int func(int num) {
+    if(num == 1) {
+        return 1;
+    } else {
+        return num * func(num-1);
+    }
+}
